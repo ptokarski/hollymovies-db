441490a78a30e2338108fc6c1c889e3551d5b4e4
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2021-01-19 17:30:28 +0100

    Factor out partial functions.

--- manage.py
+++ manage.py
@@ -17,51 +17,63 @@ def disposable_cursor(connection):
         cursor.close()
 
 
+def initialize_data_structures(cursor):
+    cursor.execute(
+        'CREATE TABLE genre ('
+        '  id INTEGER NOT NULL, '
+        '  name VARCHAR NOT NULL, '
+        '  PRIMARY KEY (id)'
+        ')'
+    )
+    cursor.execute(
+        'CREATE TABLE movie ('
+        '  id INTEGER NOT NULL, '
+        '  title VARCHAR(128) NOT NULL, '
+        '  genre_id INTEGER NOT NULL, '
+        '  rating INTEGER NOT NULL, '
+        '  released DATE NOT NULL, '
+        '  description VARCHAR, '
+        '  created DATETIME, '
+        '  PRIMARY KEY (id), '
+        '  FOREIGN KEY(genre_id) REFERENCES genre (id)'
+        ')'
+    )
+
+
+def load_data(cursor):
+    cursor.execute("INSERT INTO genre VALUES (1, 'Drama')")
+    cursor.execute(
+        "INSERT INTO movie VALUES ("
+        "  1, 'The Shawshank Redemption', 1, 9, DATE('1994-01-01'), "
+        "  'very good movie', DATETIME('now') "
+        ")"
+    )
+
+
+def dump_data(cursor):
+    cursor.execute('SELECT id, name FROM genre')
+    genres = cursor.fetchall()
+    for genre_id, genre_name in genres:
+        stdout.write(f'Genre {genre_id} is {genre_name}.\n')
+    cursor.execute(
+        'SELECT title, genre.name, released '
+        'FROM movie '
+        'JOIN genre ON movie.genre_id = genre.id'
+    )
+    movies = cursor.fetchall()
+    for title, genre, released in movies:
+        release_year = datetime.strptime(released, '%Y-%m-%d').year
+        stdout.write(
+            f'"{title}" is a {genre.lower()} released in {release_year}.\n'
+        )
+
+
 def main():
     with connect(DB_FILENAME) as connection, \
             disposable_cursor(connection) as cursor:
-        cursor.execute(
-            'CREATE TABLE genre ('
-            '  id INTEGER NOT NULL, '
-            '  name VARCHAR NOT NULL, '
-            '  PRIMARY KEY (id)'
-            ')'
-        )
-        cursor.execute(
-            'CREATE TABLE movie ('
-            '  id INTEGER NOT NULL, '
-            '  title VARCHAR(128) NOT NULL, '
-            '  genre_id INTEGER NOT NULL, '
-            '  rating INTEGER NOT NULL, '
-            '  released DATE NOT NULL, '
-            '  description VARCHAR, '
-            '  created DATETIME, '
-            '  PRIMARY KEY (id), '
-            '  FOREIGN KEY(genre_id) REFERENCES genre (id)'
-            ')'
-        )
-        cursor.execute("INSERT INTO genre VALUES (1, 'Drama')")
-        cursor.execute(
-            "INSERT INTO movie VALUES ("
-            "  1, 'The Shawshank Redemption', 1, 9, DATE('1994-01-01'), "
-            "  'very good movie', DATETIME('now') "
-            ")"
-        )
-        cursor.execute('SELECT id, name FROM genre')
-        genres = cursor.fetchall()
-        for genre_id, genre_name in genres:
-            stdout.write(f'Genre {genre_id} is {genre_name}.\n')
-        cursor.execute(
-            'SELECT title, genre.name, released '
-            'FROM movie '
-            'JOIN genre ON movie.genre_id = genre.id'
-        )
-        movies = cursor.fetchall()
-        for title, genre, released in movies:
-            release_year = datetime.strptime(released, '%Y-%m-%d').year
-            stdout.write(
-                f'"{title}" is a {genre.lower()} released in {release_year}.\n'
-            )
+        initialize_data_structures(cursor)
+        load_data(cursor)
+        dump_data(cursor)
 
 
 if __name__ == "__main__":
